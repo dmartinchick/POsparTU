fcc77c9257f4c148a218bb94f29770d5a0ba512c
dmartinchick <dmartinchick@gmail.com>
2023-02-16 19:47:47 +0300

    add aiogram start config

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from loguru import logger
 from aiogram import Bot, Dispatcher, executor, types
 
+from data.config import load_config
+
 
 def main():
-    pass
+    config = load_config('.env')
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher(bot)
 
 
 if __name__ == '__main__':
